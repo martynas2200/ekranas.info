b541ae88fb46770d85302af158d4403d71436603
Martin <martynas2200@gmail.com>
2020-02-24 23:01:43 +0200

    Updated account page design, fixed no username

--- admin-part/src/app/settings/users/users.component.ts
+++ admin-part/src/app/settings/users/users.component.ts
@@ -4,6 +4,14 @@ import { HttpClient } from '@angular/common/http';
 import { SnackbarService } from 'src/app/ui/snackbar/snackbar.service';
 import { LoginService } from 'src/app/login.service';
 import { MatDialog } from '@angular/material/dialog';
+  // const MONTH_NAMES_EN = [
+  //   'January', 'February', 'March', 'April', 'May', 'June',
+  //   'July', 'August', 'September', 'October', 'November', 'December'
+  // ];
+  const MONTH_NAMES_LT = [
+    'Sausio', 'Vasario', 'Kovo', 'Balandžio', 'Gegužės', 'Birželio',
+    'Liepos', 'Rugpjūčio', 'Rugsėjo', 'Spalio', 'Lapkričio', 'Gruodžio'
+  ];
 interface User {
   email: string;
   id: number;
@@ -69,40 +77,72 @@ export class UsersComponent implements OnInit {
   returnUserIndexById(id: number) {
     return this.users.findIndex(x => x.id === id);
   }
-  // timeAgo(date: Date) {
-  //   const diff = new Date((new Date().getTime()) - date.getTime());
-  //   if (diff.geMonth > 0) {
-  //     return diff.m + '&nbsp;'.linksniai(diff.m, 'mėnesį', 'mėnesius', 'mėnesių');
-  //   }
-  //   else if (diff.d > 0) {
-  //     return diff.d + '&nbsp;'.linksniai(diff.d, 'dieną', 'dienas', 'dienų');
-  //   }
-  //   else if (diff.h > 0) {
-  //     return diff.h + '&nbsp;'.linksniai(diff.h, 'valandą', 'valandas', 'valandų');
-  //   }
-  //   else if (diff.i > 0) {
-  //     return diff.i + '&nbsp;'.linksniai(diff.i, 'minutę', 'minutes', 'minučių');
-  //   }
-  //   else if (diff.s > 0) {
-  //     return diff.s + '&nbsp;'.linksniai(diff.s, 'sekundę', 'sekundes', 'sekundžių');
-  //   }
-
-  // }
-  // linksniai(sk, aa, bb, cc) {
-  //   // aa minutė
-  //   // bb minutės
-  //   // cc minučių
-  //   // tslint:disable-next-line: triple-equals
-  //   if (sk >= 11 && sk <= 19) {
-  //     return cc;
-  //   } else if (sk === 1 || sk % 10 === 1) {
-  //     return aa;
-  //   } else if (sk % 10 === 0) {
-  //     return cc;
-  //   } else {
-  //     return bb;
-  //   }
-  // }
+  
+  public getFormattedDate(date, prefomattedDate = '', hideYear = false) {
+    const day = date.getDate();
+    const month = MONTH_NAMES_LT[date.getMonth()];
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    let minutes = date.getMinutes();
+  
+    if (minutes < 10) minutes = "0" + minutes;
+    if (prefomattedDate) {
+      // Yesterday / Today at 10:20
+      // return `${ prefomattedDate } at ${ hours }:${ minutes }`;
+      return `${ prefomattedDate } ${ hours }:${ minutes }`;
+    }
+  
+    if (hideYear) {
+      // 10. January at 10:20
+      // return `${ day }. ${ month } at ${ hours }:${ minutes }`;
+      return `${ month } ${ day } ${ hours }:${ minutes }`;
+    }
+  
+    // 10. January 2017. at 10:20
+    // return `${ day }. ${ month } ${ year }. at ${ hours }:${ minutes }`;
+    return `${ year } ${ month } ${ day } d. ${ hours }:${ minutes }`;
+  }
+  
+  public timeAgo(dateParam) {
+    if (!dateParam) {
+      return null;
+    }
+  
+    const date = typeof dateParam === 'object' ? dateParam : new Date(dateParam);
+    const DAY_IN_MS = 86400000;
+    const today = new Date();
+    const yesterday = new Date(today.getTime() - DAY_IN_MS);
+    const seconds = Math.round((today.getTime() - date.getTime()) / 1000);
+    const minutes = Math.round(seconds / 60);
+    const isToday = today.toDateString() === date.toDateString();
+    const isYesterday = yesterday.toDateString() === date.toDateString();
+    const isThisYear = today.getFullYear() === date.getFullYear();
+  
+  
+    if (seconds < 30) {
+      return 'dabar';
+      // return 'now';
+    } else if (seconds < 60) {
+      return `prieš ${ seconds } sekundes`;
+      // return `${ seconds } seconds ago`;
+    } else if (seconds < 90) {
+      return 'prieš';
+      // return 'about a minute ago';
+    } else if (minutes < 60) {
+      return `prieš ${ minutes } minutes`;
+      // return `${ minutes } minutes ago`;
+    } else if (isToday) {
+      return this.getFormattedDate(date, 'Šiandien');
+      // return this.getFormattedDate(date, 'Today'); // Today at 10:20
+    } else if (isYesterday) {
+      return this.getFormattedDate(date, 'Vakar');
+      // return this.getFormattedDate(date, 'Yesterday'); // Yesterday at 10:20
+    } else if (isThisYear) {
+      return this.getFormattedDate(date, '', true); // 10. January at 10:20
+    }
+  
+    return this.getFormattedDate(date); // 10. January 2017. at 10:20
+  }
   getRoleName(role) {
     switch (role) {
       case 0:
